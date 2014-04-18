1241a5191ab73238897c0ba97387dfef146c75db
luizrogeriocn <luizrogeriocn@gmail.com>
2014-04-18 16:13:12 -0300

    fix regex to match all proximates values

--- parse_csv_xml.js
+++ parse_csv_xml.js
@@ -1,82 +1,134 @@
 var fs = require('fs');
 
-var text = fs.readFileSync('/home/roger/Documents/scrapper/res_test','utf8');
+var text = fs.readFileSync('/home/roger/Documents/scrapper/results.txt','utf8');
 var names = [];
 var proximates = [];
 var minerals = [];
+var vitamins = [];
+var lipids = [];
+var others = [];
 var foods = [];
+var name = [];
+var proximates_not_found = 0;
+var minerals_not_found = 0;
 
-var getFoods = function(){
-	var re = /EDT\n((.|\n(?!Source))*)/g;
-	var s = text
+var getFoods = function(textParam){
+	var re = /Basic\sReport\n((.|\n(?!==END==))*)/g;
+	var s = textParam;
 	var cont = 0;
 	var m;
 
 	do {
 	    m = re.exec(s);
 	    if (m) {
-	    	cont++
-	    	foods.push(m[1]);
-	        console.log("Getting #"+cont);
+	    	cont++;
+	    	var food = {};
+	    	food.name = getName(m[1]);
+	    	food.proximates = getProximates(m[1]);
+	    	foods.push(food);
+	        console.log("Getting food #"+cont);
 	    }
 	} while (m);
-	console.log("Foods obtained: "+foods.length);
-}
+};
 
-var getName = function(foodsParam){
+var getName = function(param){
 	var re = /"Nutrient data for:\s\s\d{5},(.*)"/g;
+
+    var m = re.exec(param);
+    if (m)
+    	return(m[1]);
+    console.log("ERROR - NO NAME FOUND");
+};
+
+var getProximates = function(param){
+	var re = /Proximates\n((.*\n")*(.*\n))/g;
+
+	var m = re.exec(param);
+    if (m)
+    	return(m[1]);
+    console.log("ERROR - NO PROXIMATE FOUND");
+    proximates_not_found++;
+};
+
+var getMinerals = function(param){
+	var re = /Minerals\n((.*\n")*(.*\n))/g;
+
+	var m = re.exec(param);
+    if (m)
+    	return(m[1]);
+    console.log("ERROR - NO MINERAL FOUND");
+    minerals_not_found++;
+};
+
+var getVitamins = function(foodsParam){
+	var re = /Vitamins\n((.*\n)*)Lipids/g;
 	var s = foodsParam;
 	var cont = 0;
 	var m;
+
 	for(var i = 0; i < foodsParam.length; i++){
 		do {
 		    m = re.exec(s[i]);
 		    if (m) {
-		    	cont++
-		    	names.push(m[1]);
-		        console.log("Getting #"+cont);
+		    	cont++;
+		    	vitamins.push(m[1]);
+		        console.log("Getting vitamin #"+cont);
 		    }
 		} while (m);
-		console.log("Names obtained: "+names.length);
 	}
 };
 
-var getNames = function(){
-	var re = /"Nutrient data for:\s\s\d{5},(.*)"/g;
-	var s = text
+var getLipids = function(foodsParam){
+	var re = /Lipids\n((.*\n)*)Other/g;
+	var s = foodsParam;
 	var cont = 0;
 	var m;
 
-	do {
-	    m = re.exec(s);
-	    if (m) {
-	    	cont++
-	    	names.push(m[1]);
-	        console.log("Getting #"+cont);
-	    }
-	} while (m);
-	console.log("Names obtained: "+names.length);
+	for(var i = 0; i < foodsParam.length; i++){
+		do {
+		    m = re.exec(s[i]);
+		    if (m) {
+		    	cont++;
+		    	lipids.push(m[1]);
+		        console.log("Getting lipid #"+cont);
+		    }
+		} while (m);
+	}
 };
 
-var getProximates = function(){
-	var re = /Proximates\n((.*\n)*)Minerals/g;
-	var s = text
+var getOthers = function(foodsParam){
+	var re = /Other\n((.|\n(?!Source))*)/g;
+	var s = foodsParam;
 	var cont = 0;
 	var m;
 
-	do {
-	    m = re.exec(s);
-	    if (m) {
-	    	cont++
-	    	proximates.push(m[1]);
-	        console.log("Getting proximate #"+cont);
-	    }
-	} while (m);
-	console.log("Proximates obtained: "+proximates.length);
+	for(var i = 0; i < foodsParam.length; i++){
+		do {
+		    m = re.exec(s[i]);
+		    if (m) {
+		    	cont++;
+		    	others.push(m[1]);
+		        console.log("Getting others #"+cont);
+		    }
+		} while (m);
+	}
 };
 
-getFoods();
-//console.log(foods);
-getName(foods);
-console.log(names);
+getFoods(text);
+/*getNames(foods);
+getProximates(foods);
+getMinerals(foods);
+getVitamins(foods);
+getLipids(foods);
+getOthers(foods);*/
+console.log("--------------------------------------------");
+console.log("Foods obtained: "+foods.length);
+console.log(foods[3000]);
+/*console.log("Names obtained: "+names.length);
+console.log("Proximates obtained: "+proximates.length);
+console.log("Minerals obtained: "+minerals.length);
+console.log("Vitamins obtained: "+vitamins.length);
+console.log("Lipids obtained: "+lipids.length);
+console.log("Others obtained: "+others.length);*/
+console.log(nf);
 
--- scrapper.js
+++ scrapper.js
@@ -1,18 +1,16 @@
-http = require('http');
+var request = require('request');
 var fs = require('fs');
 
 var url_begin = "http://ndb.nal.usda.gov/ndb/foods/show/";
 var url_end = "?format=Abridged&reportfmt=csv";
 
-for(var i = 1; i <= 8463; i++){
-	http.get(url_begin+i+url_end, function(res) {
-	  res.on('data', function (chunk) {
-	      fs.appendFile('/home/roger/Documents/scrapper/result.txt', chunk, function (err) {
-	        if (err) throw err;
-	        console.log('The "data to append" was appended to file!');
-	      });
-	    });
-	}).on('error', function(e) {
-	  console.log("Got error: " + e.message);
+for(var i = 1; i < 8464; i++){
+	request(url_begin+i+url_end, function (error, response, body) {
+	    if (!error) {
+	    	fs.appendFileSync('/home/roger/Documents/scrapper/results.txt', body);
+	    	fs.appendFileSync('/home/roger/Documents/scrapper/results.txt', '\n==END==\n');
+	    } else {
+	        console.log("error retrieving data");
+	    }
 	});
 }
