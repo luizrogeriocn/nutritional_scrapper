5e6c62d0dc94afe10d928ad2234d3c2d25a0d767
luizrogeriocn <luizrogeriocn@gmail.com>
2014-04-19 05:29:11 -0300

    complete data extraction task

--- parser_csv_xml.js
+++ parser_csv_xml.js
@@ -1,9 +1,15 @@
 var fs = require('fs');
 var text = fs.readFileSync('results.txt','utf8');
 
+var get_nome = function(param){
+	var reg_exp = /"Nutrient data for:\s\s\d{5},(.*)"/g;
+	var m = reg_exp.exec(param);
+    if (m)
+    	return m[1];
+}
+
 var get_attributes = function(food, n){
 	var reg_exps = [];
-	reg_exps.push(/"Nutrient data for:\s\s\d{5},(.*)"/g);
 	reg_exps.push(/Proximates\n((.*\n")*(.*\n))/g);
 	reg_exps.push(/Minerals\n((.*\n")*(.*\n))/g);
 	reg_exps.push(/Vitamins\n((.*\n")*(.*\n))/g);
@@ -12,22 +18,22 @@ var get_attributes = function(food, n){
 
 	var m = reg_exps[n].exec(food);
     if (m)
-    	return(m[1]);
+    	return(get_nutrients(m[1]));
 };
 
-var get_values = function(param){
+var get_amounts = function(param){
 	var re = /[^0-9],(([0-9]|\.)*),/g;
 	var s = param;
 	var m;
-	var values = [];
+	var amounts = [];
 
 	do {
 		m = re.exec(s);
 		if(m){
-			values.push(m[1]);
+			amounts.push(m[1]);
 		}
 	} while(m);
-	return values;
+	return amounts;
 };
 
 var get_units = function(param){
@@ -60,6 +66,22 @@ var get_names = function(param){
 	return names;
 };
 
+var get_nutrients = function(param){
+	var nomes = get_names(param);
+	var units = get_units(param);
+	var amounts = get_amounts(param);
+	var nutrients = [];
+
+	for(var i = 0; i < nomes.length; i++){
+		var nutrient = {};
+		nutrient.nome = nomes[i];
+		nutrient.unit = units[i];
+		nutrient.amount = amounts[i];
+		nutrients.push(nutrient);
+	}
+	return nutrients;
+};
+
 var get_foods = function(param){
 	var re = /Basic\sReport\n((.|\n(?!==END==))*)/g;
 	var s = param;
@@ -72,12 +94,12 @@ var get_foods = function(param){
 	    if (m) {
 	    	cont++;
 	    	var food = {};
-	    	food.nome = get_attributes(m[1], 0);
-	    	food.proximates = get_attributes(m[1], 1);
-	    	food.minerals = get_attributes(m[1], 2);
-	    	food.vitamins = get_attributes(m[1], 3);
-	    	food.lipids = get_attributes(m[1], 4);
-	    	food.other = get_attributes(m[1], 5);
+	    	food.nome = get_nome(m[1]);
+	    	food.proximates = get_attributes(m[1], 0);
+	    	food.minerals = get_attributes(m[1], 1);
+	    	food.vitamins = get_attributes(m[1], 2);
+	    	food.lipids = get_attributes(m[1], 3);
+	    	food.other = get_attributes(m[1], 4);
 	    	foods.push(food);
 	        console.log("Getting food #"+cont);
 	    }
@@ -87,9 +109,3 @@ var get_foods = function(param){
 
 var result = get_foods(text);
 console.log(result[321]);
-var names = get_names(result[321].proximates);
-console.log(names);
-var units = get_units(result[321].proximates);
-console.log(units);
-var values = get_values(result[321].proximates);
-console.log(values);
